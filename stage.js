ac3c29db81d682dadce8413a24b7281f817336b0
Asaf Ohayon <asaf@sysbind.co.il>
2022-04-28 18:22:51 +0300

    start with intersections

--- stage.js
+++ stage.js
@@ -6,12 +6,18 @@ class Stage {
         this.actors = new SVG.List()
     }
 
-    add(el) {
-        this.actors.push(el)
+    add(el) {        
         this.draw.add(el)
+
+        for (const actor of this.actors) {            
+            window.console.log('checking collistion with '  + actor)
+            
+        }
+        this.actors.push(el)
     }
 }
 
 var stage = new Stage()
 stage.add(new SVG.Rect().size(200,200))
+stage.add(new SVG.Rect().size(100,300))
 
