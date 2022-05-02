4980df211d46e939938ac5bd2d46f7b0372e6204
Asaf Ohayon <asaf@sysbind.co.il>
2022-05-02 21:29:26 +0300

    add button for testing

--- stage.js
+++ stage.js
@@ -17,6 +17,7 @@ class Stage {
 
         var target_bbox = el.bbox()
         var max_y = 0
+        var cur_y = 0
         for (const actor of this.actors) {            
 
             let real_bbox = actor.remember('target_bbox')
@@ -32,14 +33,16 @@ class Stage {
                 window.console.log('COLLISIONS!')
                 // move X ?
                 if (real_bbox.x2 + target_bbox.width  < (stage_width  + actor_spacing)) {
+                    target_bbox.y = cur_y
+                    target_bbox.y2 = target_bbox.y + target_bbox.height
                     target_bbox.x = real_bbox.x2 + actor_spacing
                     target_bbox.x2 = target_bbox.x + target_bbox.width
-                    window.console.log('move x')
                 } else {
-                    target_bbox.y = max_y  + actor_spacing
+                    target_bbox.y = max_y  + actor_spacing                    
                     target_bbox.y2 = target_bbox.y + target_bbox.height
                     target_bbox.x = 0
                     target_bbox.x2 = target_bbox.x + target_bbox.width
+                    this.cur_y = max_y                    
                 }
                 el.animate().move(target_bbox.x, target_bbox.y)
             }
@@ -64,7 +67,7 @@ stage.add(new SVG.Rect().size(100,100).attr({
 , 'stroke-width': 10
 }))
 
-stage.add(new SVG.Rect().size(100,100).attr({
+stage.add(new SVG.Rect().size(200,200).attr({
   fill: '#f60'
 , 'fill-opacity': 0.9
 , stroke: '#636'
@@ -86,10 +89,12 @@ stage.add(new SVG.Rect().size(240,240).attr({
 }))
 
 
-stage.add(new SVG.Rect().size(150,150).attr({
-  fill: '#3f0'
-, 'fill-opacity': 0.1
-, stroke: '#969'
-, 'stroke-width': 10
-}))
 
+function add_one() {
+    stage.add(new SVG.Rect().size(150,150).attr({
+        fill: '#3f0'
+        , 'fill-opacity': 0.1
+        , stroke: '#969'
+        , 'stroke-width': 10
+    }))
+}
