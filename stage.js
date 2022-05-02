6afa6a42e519d8373f6c9361317f43f1739add04
Asaf Ohayon <asaf@sysbind.co.il>
2022-05-02 21:12:39 +0300

    reasonable layouting

--- stage.js
+++ stage.js
@@ -2,7 +2,7 @@
 
 const stage_width = 800;
 const stage_height = 600;
-const actor_spacing = 10;
+const actor_spacing = 20;
 
 const sleep = ms => new Promise(r => setTimeout(r, ms))
 
@@ -16,12 +16,13 @@ class Stage {
         this.draw.add(el)
 
         var target_bbox = el.bbox()
-        var xcount = 0        
-        for (const actor of this.actors) {
-            xcount += 1
-            let xspacing = xcount * actor_spacing
-            window.console.log('considering ' + actor)            
+        var max_y = 0
+        for (const actor of this.actors) {            
+
             let real_bbox = actor.remember('target_bbox')
+            if (real_bbox.y2 > max_y)
+                max_y = real_bbox.y2
+            
             window.console.log(real_bbox)
             window.console.log(target_bbox)
             if (target_bbox.x < real_bbox.x2 &&
@@ -30,16 +31,17 @@ class Stage {
                 target_bbox.y2 > real_bbox.y) {
                 window.console.log('COLLISIONS!')
                 // move X ?
-                if (real_bbox.x2 + target_bbox.width  < (stage_width  + xspacing)) {
-                    target_bbox.x = real_bbox.x2 + xspacing
+                if (real_bbox.x2 + target_bbox.width  < (stage_width  + actor_spacing)) {
+                    target_bbox.x = real_bbox.x2 + actor_spacing
                     target_bbox.x2 = target_bbox.x + target_bbox.width
+                    window.console.log('move x')
                 } else {
-                    target_bbox.y = real_bbox.height  + actor_spacing
+                    target_bbox.y = max_y  + actor_spacing
                     target_bbox.y2 = target_bbox.y + target_bbox.height
-                    xcount = 0
+                    target_bbox.x = 0
+                    target_bbox.x2 = target_bbox.x + target_bbox.width
                 }
-                window.console.log(target_bbox)
-                window.console.log(el.animate().move(target_bbox.x, target_bbox.y))
+                el.animate().move(target_bbox.x, target_bbox.y)
             }
         }
         el.remember('target_bbox', target_bbox)
@@ -48,9 +50,14 @@ class Stage {
 }
 
 var stage = new Stage()
-stage.add(new SVG.Rect().size(200,200))
+stage.add(new SVG.Rect().size(200,200).attr({
+  fill: '#f6c'
+, 'fill-opacity': 0.7
+, stroke: '#322'
+, 'stroke-width': 10
+}))
 
-stage.add(new SVG.Rect().size(100,300).attr({
+stage.add(new SVG.Rect().size(100,100).attr({
   fill: '#f06'
 , 'fill-opacity': 0.9
 , stroke: '#333'
@@ -64,17 +71,25 @@ stage.add(new SVG.Rect().size(100,100).attr({
 , 'stroke-width': 10
 }))
 
-stage.add(new SVG.Rect().size(400,100).attr({
+stage.add(new SVG.Rect().size(300,300).attr({
   fill: '#f60'
 , 'fill-opacity': 0.9
 , stroke: '#636'
 , 'stroke-width': 10
 }))
 
-stage.add(new SVG.Rect().size(200,200).attr({
+stage.add(new SVG.Rect().size(240,240).attr({
   fill: '#f30'
 , 'fill-opacity': 0.1
 , stroke: '#996'
+, 'stroke-width': 10
+}))
+
+
+stage.add(new SVG.Rect().size(150,150).attr({
+  fill: '#3f0'
+, 'fill-opacity': 0.1
+, stroke: '#969'
 , 'stroke-width': 10
 }))
 
