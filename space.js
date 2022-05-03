aea033fc75a82399fc96ba6fbe5c81c2df604acc
Asaf Ohayon <asaf@sysbind.co.il>
2022-05-03 18:49:08 +0300

    somewhat working example of spaces algo

--- space.js
+++ space.js
@@ -1,6 +1,6 @@
 // space.js
 
-const actor_spacing = 20;
+const actor_spacing = 10;
 
 class Space {
     constructor(x, y, x2, y2) {
@@ -9,7 +9,8 @@ class Space {
         this.x2 = x2
         this.y2 = y2
 
-        this.spaces = null
+        this.spaces = new Set()
+        this.center = false
     }
 
     width() {
@@ -21,15 +22,62 @@ class Space {
     }
 
     place(el) {
+        window.console.log('place', this)
         var x, y = 0
         // place center
         if (this.width() / el.bbox().width < 2) { 
-            x = (this.width() - el.bbox().width) / 2
-            y = (this.height() - el.bbox().height) / 2
+            x = this.x + (this.width() - el.bbox().width) / 2
+            y = this.y + (this.height() - el.bbox().height) / 2
+            this.devide_space_center(x, y, el.bbox().width, el.bbox().height)
         } else {  // place corner
             x = this.x +  actor_spacing
             y = this.y + actor_spacing
+            this.devide_space_corner(x, y, el.bbox().width, el.bbox().height)
         }
-        el.animate().move(x, y)
+        el.animate().move(x, y)        
+    }
+
+    devide_space_corner(x, y, width, height) {
+        this.spaces.add(new Space(x + width, y, this.x2, y + height))
+        this.spaces.add(new Space(x, y + height, x + width, this.y2))
+        this.spaces.add(new Space(x + width, y + height, this.x2, this.y2))
+    }
+
+    devide_space_center(x, y, width, height) {
+        window.console.log('CENTER')
+        this.center = true
+    }
+
+    find_space(bbox) {
+        if (this.center)
+            return null
+        
+        window.console.log('find_space bbox', bbox)
+        if ((this.spaces.size == 0)) {
+            window.console.log('find_space return this', this)
+            return this
+        }
+
+        var space = null
+        for (const entry of this.spaces.entries() ) {
+            var curspace = entry[0].find_space(bbox)
+            if (curspace == null)
+                continue
+            
+            if ((curspace.width < bbox.width) || (curspace.height < bbox.height)) {
+                window.console.log('skipping small space', curspace)
+                continue
+            }
+
+            if (space == null) {
+                space = curspace
+                continue
+            }
+            
+            if (curspace.width() > space.width() || curspace.height() > space.height() )
+                space = curspace
+        }
+        window.console.log('find_space return', space)
+        return space
     }
 }
--- stage.js
+++ stage.js
@@ -12,13 +12,22 @@ class Stage {
 
     add(el) {        
         this.draw.add(el)
-        var space = this.find_space(el.bbox())
+        var space = this.root_space.find_space(el.bbox())
         space.place(el)
         this.actors.push(el)        
     }
 
-    find_space(bbox) {
-        return this.root_space
+    draw_spaces(space = this.root_space) {
+        this.draw.rect(space.width(), space.height()).move(space.x, space.y).attr({
+            fill: '#3f0'
+            , 'fill-opacity': 0
+            , stroke: '#969'
+            , 'stroke-width': 1
+        })
+
+        for (const entry of space.spaces.entries()) {            
+            this.draw_spaces(entry[0])
+        }
     }
 }
 
@@ -30,7 +39,7 @@ stage.add(new SVG.Rect().size(200,200).attr({
   fill: '#f6c'
 , 'fill-opacity': 0.7
 , stroke: '#322'
-, 'stroke-width': 10
+, 'stroke-width': 5
 }))
 
 // stage.add(new SVG.Rect().size(100,100).attr({
@@ -66,8 +75,12 @@ stage.add(new SVG.Rect().size(200,200).attr({
 function add_one() {
     stage.add(new SVG.Rect().size(150,150).attr({
         fill: '#3f0'
-        , 'fill-opacity': 0.1
+        , 'fill-opacity': 0.4
         , stroke: '#969'
-        , 'stroke-width': 10
+        , 'stroke-width': 5
     }))
+}
+
+function draw_spaces() {
+    stage.draw_spaces()
 }
