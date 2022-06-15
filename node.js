15bd12c9184678dfafccf0b7e2d0db4f7306d8a2
Asaf Ohayon <asaf@sysbind.co.il>
2022-06-15 23:34:09 +0300

    front: start refactoring node

--- node.js
+++ node.js
@@ -17,58 +17,64 @@ class KNode {
         this.alloc_mem = alloc_mem
 
         // total graphic width & height
-        const width = this.alloc_cpu / 30 + node_panel_size
-        const height = this.alloc_mem / 1000 / 100 + 2*node_panel_size
+        this.width = this.alloc_cpu / 30 + node_panel_size
+        this.height = this.alloc_mem / 1000 / 100 + 2*node_panel_size
 
         // svg container
-        this.svg = new SVG.Svg().size(width, height)
+        this.svg = new SVG.Svg().size(this.width, this.height)
         this.svg.click(function(e) {
             var audio = new Audio('sounds/menu-selection-click.wav')
             audio.play()
         });
 
         // main rect
-        this.svg.rect(width, height - node_panel_size).attr({
+        this.svg.rect(this.width, this.height - node_panel_size).attr({
             fill: '#3f0'
             , 'fill-opacity': 0.4
             , stroke: '#969'
-            , 'stroke-width': 3
+            , 'stroke-this.width': 3
             ,'rx': "20px"
             ,'ry': "10px"}).move(0, node_panel_size)
 
         // title
-        this.svg.text(this.name).size(width - node_panel_size, node_panel_size).move(10,10).attr({
+        this.svg.text(this.name).size(this.width - node_panel_size, node_panel_size).move(10,10).attr({
             lengthAdjust: "spacingAndGlyphs"
         })
 
         // right panel - display allocatable memory
-        this.svg.rect(node_panel_size, height * 0.5).move(width - node_panel_size, height * 0.25).attr({
+        this.svg.rect(node_panel_size, this.height * 0.5).move(this.width - node_panel_size, this.height * 0.25).attr({
                     fill: '#f3f'
                     , 'fill-opacity': 0.1
                     , stroke: '#996'
-                    , 'stroke-width': 1
+                    , 'stroke-this.width': 1
         })
         
         const mem_gig = Math.round(this.alloc_mem / 1000 / 1000 )
-        this.svg.text(`${mem_gig}Gi`).size(node_panel_size, height * 0.5).move(width - node_panel_size * 0.6, height * 0.3).attr({
+        this.svg.text(`${mem_gig}Gi`).size(node_panel_size, this.height * 0.5).move(this.width - node_panel_size * 0.6, this.height * 0.3).attr({
             stroke: '#0362fc',
             fill: '#0362fc',
             "writing-mode": "tb"            
         })
 
         // bottom panel - display allocatable cpu
-        this.svg.rect(width * 0.5, node_panel_size).move(width * 0.25, height - node_panel_size).attr({
+        this.bottomPannel()
+    }
+
+
+    bottomPannel() {
+        this.svg.rect(this.width * 0.5, node_panel_size).move(this.width * 0.25, this.height - node_panel_size).attr({
             fill: 'white'
                     , 'fill-opacity': 0.1
                     , stroke: '#699'
-                    , 'stroke-width': 1
+                    , 'stroke-this.width': 1
         })
-        this.svg.text(`${this.alloc_cpu}Mi`).size(width * 0.5, node_panel_size).move(width * 0.3, height - node_panel_size * 0.75).attr({
+        this.svg.text(`${this.alloc_cpu}Mi`).size(this.width * 0.5, node_panel_size).move(this.width * 0.3, this.height - node_panel_size * 0.75).attr({
             stroke: '#0362fc',
             fill: '#0362fc'
-        })
+        })        
     }
-
+    
+    // SVG: Getter
     get SVG() {
         return this.svg
     }
